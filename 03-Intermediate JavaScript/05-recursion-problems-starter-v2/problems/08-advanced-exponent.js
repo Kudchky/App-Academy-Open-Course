9c72eb4115b2fa99f6b4ff4f6ca3b604b80879ef
Paul Castillo <rpcastilloy@gmail.com>
2023-11-16 20:51:29 -0500

    Done exersice 08

--- 03-Intermediate JavaScript/05-recursion-problems-starter-v2/problems/08-advanced-exponent.js
+++ 03-Intermediate JavaScript/05-recursion-problems-starter-v2/problems/08-advanced-exponent.js
@@ -42,7 +42,12 @@ times `advancedExponent` is being recursively called.
 
 function advancedExponent(b, n) {
   // your code here
+  if(n === 0) return 1;
+  if(n === 1) return b;
+
+  return n % 2 === 0 ? advancedExponent(b, n / 2) ** 2 : b * (advancedExponent(b, (n - 1) / 2) ** 2);
 }
+
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
